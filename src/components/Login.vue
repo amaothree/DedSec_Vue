4c81ca3bb2d74071b19a210b9314ba1dc37c200a
amao_three <tigersui@sina.com>
2019-04-07 19:17:46 +0800

    fix bugs

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -50,44 +50,44 @@ export default {
       hidden: true,
       errorMessage: {
         phone: this.$t('login.Account'),
-        password: this.$t('login.Password'),
+        password: this.$t('login.Password')
       }
     }
   },
   mounted () {
     if (this.$cookie.get('lng') === '0') {
-      this.locale = 'cn';
-      this.lang = 'ENG';
+      this.locale = 'cn'
+      this.lang = 'ENG'
     } else {
-      this.locale = 'en';
-      this.lang = '中文';
+      this.locale = 'en'
+      this.lang = '中文'
     }
-    this.$cookie.set('lng', this.locale === 'cn' ? '0' : '1', 1);
+    this.$cookie.set('lng', this.locale === 'cn' ? '0' : '1', 1)
   },
   watch: {
     locale (val) {
-      this.$i18n.locale = val;
-      console.log('locale', val);
+      this.$i18n.locale = val
+      console.log('locale', val)
     }
   },
   methods: {
     changeLang () {
       // 增加传入语言
       if (this.locale === 'cn') {
-        this.lang = '中文';
-        this.locale = 'en';
+        this.lang = '中文'
+        this.locale = 'en'
       } else {
-        this.lang = 'ENG';
-        this.locale = 'cn';
+        this.lang = 'ENG'
+        this.locale = 'cn'
       }
-      this.$cookie.set('lng', this.locale === 'cn' ? '0' : '1', 1);
-      window.location.reload(); // 进行刷新改变cookie里的值
+      this.$cookie.set('lng', this.locale === 'cn' ? '0' : '1', 1)
+      window.location.reload() // 进行刷新改变cookie里的值
     },
     onBlurCheckPassword (name) {
       console.log(name, 'check name')
       const value = this[name]
       if (!value || value.length < 1) {
-        return (this.errorMessage[name] =  this.$t('login.ps'))
+        return (this.errorMessage[name] = this.$t('login.ps'))
       } else {
         this.errorMessage[name] = ''
       }
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,7 +1,7 @@
 import VueI18n from 'vue-i18n'
 import Vue from 'vue'
 
-Vue.use(VueI18n);
+Vue.use(VueI18n)
 
 const i18n = new VueI18n({
   locale: 'en', // 语言标识
@@ -9,6 +9,6 @@ const i18n = new VueI18n({
     'cn': require('./lang/cn'), // 中文语言包
     'en': require('./lang/en') // 英文语言包
   }
-});
+})
 
 export default i18n
